a6c3781c04ed9762026fa90bf1ab9e55a90e2334
ShethJay <jay.sheth@upforcetech.com>
2019-10-16 17:30:23 +0530

    propTypes test cases

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
+import Header from './components/header';
+import Headline from './components/headline';
+import './app.scss';
+
+const tempArr = [{
+  fName: 'Jay',
+  lName: 'Sheth',
+  email: 'Jay@gmail.com',
+  age: 25,
+  onlineStatus: true
+}];
+
 function App() {
   return (
     <div className="App">
-      Test
+      <Header />
+      <section className="main">
+        <Headline 
+          header="posts" 
+          desc="Click the button the to render posts"
+          tempArr={tempArr} />  
+      </section> 
     </div>
   );
 }
